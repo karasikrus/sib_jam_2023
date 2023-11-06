b6d30de9915326e7bce7432491b683c4d43a43d5
karasikrus <karasikrus@gmail.com>
2023-11-06 18:42:04 +0300

    monologues, backgrounds

--- monologues/monologue_player.gd
+++ monologues/monologue_player.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 @export var monologues : Array[Monologue] = []
 @onready var label = %Label
 @onready var audio_stream_player = %AudioStreamPlayer
+@onready var boring_timer = $BoringTimer
 
 var is_timer_active = false
 var timer : float = 0
@@ -10,6 +11,7 @@ var played: Array[bool] = []
 
 func _ready():
 	audio_stream_player.finished.connect(on_dialogue_end)
+	boring_timer.timeout.connect(play_boring_phrase)
 	played.resize(monologues.size())
 	played.fill(false)
 	
@@ -31,6 +33,7 @@ func play_monologue(id: int) -> bool:
 	audio_stream_player.stream = mono.phrase_audio
 	audio_stream_player.play()
 	visible = true
+	boring_timer.start()
 	return true
 	
 func on_dialogue_end():
@@ -43,3 +46,10 @@ func start_timer():
 
 func update_timer(delta):
 	timer+=delta
+
+var boring_id : int = 6
+func play_boring_phrase():
+	var success = play_monologue(boring_id)
+	boring_timer.start()
+	if success:
+		boring_id+=1
